64a6884d9045a8735c665c2d4e7a5ce880c5577d
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-03-30 18:14:48 +0900

    pa

---
+++ PhonDLE_2/PhonDLE_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhonDLE_2
+//
+//  Created by Jin younkyum on 2022/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
