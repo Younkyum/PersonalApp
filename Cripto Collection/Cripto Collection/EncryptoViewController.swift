347c814d6101cc0bbd2f8d5b3d9ade1549257f8d
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-04-18 11:50:44 +0900

    CriptoCollection 0.0.0

---
+++ Cripto Collection/Cripto Collection/EncryptoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EncryptoViewController.swift
+//  Cripto Collection
+//
+//  Created by Jin younkyum on 2022/04/18.
+//
+
+import UIKit
+
+class EncryptoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+}
---
+++ Cripto Collection/Cripto Collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cripto Collection
+//
+//  Created by Jin younkyum on 2022/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
